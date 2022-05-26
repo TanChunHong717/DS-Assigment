5372c5bebf9a0fdf7d957d063a3138ce896b370e
ainaiii <92796285+ainaiii@users.noreply.github.com>
2022-05-26 14:55:54 +0800

    Add files via upload

---
+++ Titan/Titan.java
@@ -0,0 +1,55 @@
+package Titan;
+
+
+
+
+
+/*
+ * Click nfbs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nfbs://nbhost/SystemFileSystem/Templates/Classes/Class.java
+ */
+/**
+ *
+ * @author AINA SOFEA AZHAR
+ */
+public abstract class Titan {
+   int dangerRisk;
+}
+            
+           
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
+            
+            
+ 
+    
+
+
+    
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
